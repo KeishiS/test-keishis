29c7073a6feb61d8713a496993d4a3055361d5eb
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2025-11-18 01:25:13 +0900

    updated for julia

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
+#[cfg(feature = "python")]
 use pyo3::prelude::*;
 
+fn sum_as_str(a: i64, b: i64) -> String {
+    (a + b).to_string()
+}
+
+#[cfg(feature = "julia")]
+#[unsafe(no_mangle)]
+pub extern "C" fn mysum(a: i64, b: i64) -> i64 {
+    a + b
+}
+
 /// A Python module implemented in Rust.
+#[cfg(feature = "python")]
 #[pymodule]
 #[pyo3(name = "_test_keishis")]
 mod test_keishis {
+    use super::sum_as_str;
     use pyo3::prelude::*;
 
-    /// Formats the sum of two numbers as string.
     #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
+    fn sum_as_string(a: i64, b: i64) -> PyResult<String> {
+        Ok(sum_as_str(a, b))
     }
 }
